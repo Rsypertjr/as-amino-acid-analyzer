24061a734067c78688da0d90dec3048917b2d231
Richard L. Sypert Jr <rsypertjr@hotmail.com>
2026-04-21 15:24:15 -0700

    fix: update dependencies and refactor Supabase client initialization

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,14 @@
-import { createClient } from "@supabase/supabase-js";
+//import { createClient } from "@supabase/supabase-js";
+//const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL2!;
+//const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+//export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey);
+import { createBrowserClient } from "@supabase/ssr";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL2!;
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL2;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
 
-export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey);
+export const supabaseAdmin = () =>
+  createBrowserClient(
+    supabaseUrl!,
+    supabaseKey!,
+  );
